e808990ff27caa25fe2b1502d0b07a761a07eac4
Roopal <54892036+Rmittal21@users.noreply.github.com>
2019-10-11 00:33:04 +0530

    Pattern printing in C

---
+++ pattern2.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main ()
+{
+	int n, i, j, k, l;
+	printf("Enter the value of n: ");
+	scanf("%d", &n);
+	for (i=1; i<=n; i++)
+	{
+		for (j=1; j<=i; j++)
+		{
+			printf ("%d\t", j);
+		}
+		printf ("\n");
+	}
+	for (k=n-1; k>=1; k--)
+	{
+		for (l=1; l<=k; l++)
+		{
+			printf ("%d\t", l);
+		}
+		printf ("\n");
+	}
+	return 0;
+}
